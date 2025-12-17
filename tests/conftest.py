df0d1becb28b0fad8dde2fc4d7122467bba1eb4f
Abhishek Siddagalla <abhishek.s@clientservertech.com>
2025-12-17 20:14:28 +0530

    changed db config for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,11 @@ from pathlib import Path
 import django
 
 # For pytest-django, set the DJANGO_SETTINGS_MODULE to the tests-specific settings.
-os.environ['DJANGO_SETTINGS_MODULE'] = 'business_workflow.settings_test'
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "business_workflow.settings_test"
+)
+
 django.setup()
 
 # Path to your project root directory
@@ -12,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ":memory:",
     }
 }
